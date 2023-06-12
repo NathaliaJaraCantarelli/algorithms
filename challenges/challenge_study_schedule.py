6d20a6b9949cdd51a10b4841060f210f1138bb36
Nathalia <nathalia.cantarelli@edu.pucrs.br>
2023-06-12 20:00:46 -0300

    Requisito 2

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,7 +1,7 @@
 def study_schedule(permanence_period, target_time):
     if type(permanence_period) != list or type(target_time) != int:
         return None
-    
+
     quantity_students = 0
 
     for student in permanence_period:
@@ -9,5 +9,5 @@ def study_schedule(permanence_period, target_time):
             return None
         if student[0] <= target_time <= student[1]:
             quantity_students += 1
-    
+
     return quantity_students
--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,14 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    assert encrypt_message('nathalia', 2) == 'ailaht_an'
+    assert encrypt_message('nathalia', 3) == 'tan_ailah'
+    assert encrypt_message('nathalia', 9) == 'ailahtan'
+
+    with pytest.raises(TypeError):
+        encrypt_message(55, 5)
+
+    with pytest.raises(TypeError):
+        encrypt_message('nathalia', 'nathalia')
