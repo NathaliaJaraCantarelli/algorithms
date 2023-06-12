5c7424d377a50028ce13e455647948139d7f4118
Nathalia <nathalia.cantarelli@edu.pucrs.br>
2023-06-12 19:25:44 -0300

    Requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,13 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if type(permanence_period) != list or type(target_time) != int:
+        return None
+    
+    quantity_students = 0
+
+    for student in permanence_period:
+        if type(student[0]) != int or type(student[1]) != int:
+            return None
+        if student[0] <= target_time <= student[1]:
+            quantity_students += 1
+    
+    return quantity_students
