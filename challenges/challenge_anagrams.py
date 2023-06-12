68fda1ff5f307359268a7feb7629ac30176c2395
Nathalia <nathalia.cantarelli@edu.pucrs.br>
2023-06-12 20:56:54 -0300

    FIX: ajustando erros

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -10,16 +10,17 @@ def merge(left, right):
         else:
             arr.append(right[j])
             j += 1
-    
+
     arr.extend(left[i:])
     arr.extend(right[j:])
 
     return result.join(arr)
 
+
 def order(string):
     if len(string) <= 1:
         return string
-    
+
     middle = len(string) // 2
     left = order(string[:middle])
     right = order(string[middle:])
@@ -31,9 +32,9 @@ def is_anagram(first_string, second_string):
     first_string = order(first_string.lower())
     second_string = order(second_string.lower())
 
-    if len(first_string) == 0 or len(second_string) == 0 or first_string != second_string:
+    if len(first_string) == 0 or len(
+        second_string) == 0 or first_string != second_string:
         return (first_string, second_string, False)
-    
+
     else:
         return (first_string, second_string, True)
-    
