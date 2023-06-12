cd002767eb0131323587be7a6d6a75875d456bf7
Nathalia <nathalia.cantarelli@edu.pucrs.br>
2023-06-12 20:15:00 -0300

    Requisito 3

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,13 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if len(word) == 0:
+        return False
+
+    word = word.lower()
+
+    if word[low_index] != word[high_index]:
+        return False
+
+    if high_index <= low_index:
+        return True
+
+    return is_palindrome_recursive(word, low_index + 1, high_index - 1)
