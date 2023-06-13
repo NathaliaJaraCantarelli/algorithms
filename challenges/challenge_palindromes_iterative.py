59678b0ef670f391b542ba571636de002f189656
Nathalia <nathalia.cantarelli@edu.pucrs.br>
2023-06-12 21:13:57 -0300

    Requisito 56

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,7 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if len(word) == 0:
+        return False
+    word = word.lower().replace(" ", "")
+    reverse = word[::-1]
+    is_palindrome = word == reverse
+    return is_palindrome
