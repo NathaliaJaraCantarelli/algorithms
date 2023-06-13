723cf24e20190f1486bfba8a1b0db8cc8cb1c9a3
Nathalia <nathalia.cantarelli@edu.pucrs.br>
2023-06-12 21:09:57 -0300

    Requisito 5

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,15 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not nums or isinstance(nums, str) or len(nums) < 2:
+        return False
+
+    set_nums = set()
+
+    for num in nums:
+        if type(num) == str or num < 0:
+            return False
+        elif num in set_nums:
+            return num
+        else:
+            set_nums.add(num)
+
+    return False
